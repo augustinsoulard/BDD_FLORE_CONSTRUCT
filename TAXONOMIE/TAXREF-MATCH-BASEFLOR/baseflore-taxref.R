90faece48b619a425a19cd10cee976a2379429e7
Augustin Soulard <42175782+augustinsoulard@users.noreply.github.com>
2024-01-17 15:23:05 +0100

    v0.4.4

--- TAXONOMIE/TAXREF-MATCH-BASEFLOR/baseflore-taxref.R
+++ TAXONOMIE/TAXREF-MATCH-BASEFLOR/baseflore-taxref.R
@@ -12,9 +12,9 @@ if(!require("readxl")){install.packages("readxl")} ; library("readxl")
 if(!require("xlsx")){install.packages("xlsx")} ; library("xlsx")
 if(!require("tidyverse")){install.packages("tidyverse")} ; library("tidyverse")
 if(!require("rtaxref")){remotes::install_github("Rekyt/rtaxref")} ; library("rtaxref")
+
 #Chargement de baseflor_bryo
 baseflor_bryo <- read.csv("../CATMINAT/baseflor_bryo.csv",h=T)
-
 
 # Ajustement des rangs taxonomiques
 
@@ -29,34 +29,33 @@ baseflor_bryo$NOM_SIMPLE = str_split(baseflor_bryo$NOM_SIMPLE, "[:blank:]$", n =
 #Boucle de matching
 for (i in 1:nrow(baseflor_bryo)){
   cat(i,"\n")
-  t = rt_taxa_search(sciname = baseflor_bryo$NOM_SIMPLE[i],version = "16.0")
-  if(ncol(t)>1){
-    if(nrow(t[t$id==t$referenceId,])>1){t = t[t$id==t$referenceId,]}
-    if(nrow(t)>1 & nrow(t[t$rankId=="ES",])>1){t = t[t$rankId=="ES",]}
-    baseflor_bryo$CD_NOM[i] = t$referenceId[1]
-    baseflor_bryo$NOM_VALIDE[i] = t$scientificName[1]}else{
-      baseflor_bryo$CD_NOM[i] = "NOMATCH"
-      baseflor_bryo$NOM_VALIDE[i] = "NOMATCH"
-    }
+    tryCatch({
+      t = rt_taxa_search(sciname = baseflor_bryo$NOM_SIMPLE[i],version = "16.0")
+      if(ncol(t)>1){
+        #Supression des synonymes
+        if(nrow(t[t$id==t$referenceId,])>=1){t = t[t$id==t$referenceId,]}
+        #Supression des sous-espèces si nécessaire
+        if(nrow(t)>=1 & nrow(t[t$rankId=="ES",])>=1){t = t[t$rankId=="ES",]}
+        #Supression des hybrides
+        if(str_detect(baseflor_bryo$NOM_SIMPLE[i],"[:blank:]x[:blank:]")==FALSE){
+          t = t[!str_detect(t$scientificName,"[:blank:]x[:blank:]"),]
+        }
+        #Attribution des valeurs de CD_NOM et NOM_VALIDE
+        baseflor_bryo$CD_NOM[i] = t$referenceId[1]
+        baseflor_bryo$NOM_VALIDE[i] = t$scientificName[1]}else{
+          baseflor_bryo$CD_NOM[i] = "NOMATCH"
+          baseflor_bryo$NOM_VALIDE[i] = "NOMATCH"
+        }
+
+  }, error = function(e) {
+    baseflor_bryo$CD_NOM[i] = "NOMATCH"
+    baseflor_bryo$NOM_VALIDE[i] = "NOMATCH"
+  })
+
 }
-write.csv2(baseflor_bryo,"baseflor_bryoTAXREFv17.csv",row.names = F,fileEncoding = "UTF-8",na="")
 
+# Verification des différences
+difference = baseflor_bryo[baseflor_bryo$NOM_VALIDE!=baseflor_bryo$NOM_SIMPLE,]
 
-
-# Installer et charger le package stringr
-install.packages("stringr")
-library(stringr)
-
-# Exemple de dataframe
-df <- data.frame(Nom = c("Jean Dupont", "Marie-Louise Johnson", "Robert (Bob) Smith"))
-
-# Séparer avant la deuxième majuscule
-df = str_split(df$Nom, "[:blank:][:upper:]",n=2, simplify = TRUE)[,1]
-
-# Séparer avant la parenthèse
-df = str_split(df$Nom, "\\(", n = 2, simplify = TRUE)[, 1]
-
-# Afficher le résultat
-print(df)
-
+write.csv2(baseflor_bryo,"baseflor_bryoTAXREFv16.csv",row.names = F,fileEncoding = "UTF-8",na="")
 
