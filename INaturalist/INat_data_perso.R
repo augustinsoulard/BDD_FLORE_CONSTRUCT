64c5f09ac059b78d745d1ff0d7e28735015e85d1
Augustin Soulard <augustinsoulard@yahoo.fr>
2025-02-04 21:48:47 +0100

    v0.10.2

--- INaturalist/INat_data_perso.R
+++ INaturalist/INat_data_perso.R
@@ -15,3 +15,22 @@ for (i in 1:nrow(observations)){
     download.file(url, destfile = paste0("D:/INaturalist/IMG/",observations$scientific_name[i],"_",observations$id[i],"-",photos$id[j], ".jpg"), mode = "wb")
   }
 }
+
+# 660 /7586
+# Dernier import 03/02/2025
+# Api requete pour les données créées en 2025 : 
+# if(!require("httr")){install.packages("httr")} ; library("httr")
+# if(!require("jsonlite")){install.packages("jsonlite")} ; library("jsonlite")
+# url <- "https://api.inaturalist.org/v1/observations?user_login=augustinsoulard&created_year=2025&order=desc&order_by=created_at"
+# response <- GET(url)
+# # Extraire le contenu JSON
+# data <- content(response, as = "text", encoding = "UTF-8")
+# 
+# # Convertir en liste R
+# json_data <- fromJSON(data, flatten = TRUE)
+# 
+# # Vérifier la structure des données
+# str(json_data)
+# 
+# observations <- as.data.frame(json_data$results)
+# head(observations)  # Aperçu des premières lignes
